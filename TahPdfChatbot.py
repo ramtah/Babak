9379665112e5c90a77dea38ad48093fe050ab158
ramtah <41420316+ramtah@users.noreply.github.com>
2023-08-10 07:55:01 -0700

    Update TahPdfChatbot.py

--- TahPdfChatbot.py
+++ TahPdfChatbot.py
@@ -1,62 +1,104 @@
-import os
-import io
-import PyPDF2
-# from dotenv import load_dotenv
-from langchain.llms import OpenAI
-from langchain.agents import AgentType, initialize_agent, load_tools
-from langchain.callbacks import StreamlitCallbackHandler
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
 
-# load_dotenv()
-
-# Adding the title "BABAK" centered at the top
-st.markdown("<h1 style='text-align: center;'>BABAK</h1>", unsafe_allow_html=True)
-
-openai_api_key = st.text_input("Enter your OpenAI API Key:", type="password")
-llm = OpenAI(temperature=0, streaming=True, openai_api_key=openai_api_key)
-tools = load_tools(["ddg-search"])
-agent = initialize_agent(
-    tools,
-    llm,
-    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-    # verbose=True
-)
-
-# Apply custom CSS style for the "Submit" button
-st.write("""
-<style>
-    .stButton>button {
-        background-color: green;
-        color: white;
-        padding: 0.5em 1em;
-        border: none;
-        border-radius: 0.5em;
-    }
-</style>
-""", unsafe_allow_html=True)
-
-# File uploader for PDF documents
-uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
-
-# Check if a PDF file has been uploaded
-if uploaded_file is not None:
-    # Read the PDF and extract text
-    pdf_reader = PyPDF2.PdfReader(uploaded_file)
+def get_pdf_text(pdf_docs):
     text = ""
-    for page_num in range(len(pdf_reader.pages)):
-        page = pdf_reader.pages[page_num]
-        text += page.extract_text()
-    #st.write("PDF content:")
-    #st.write(text)
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
-# Text area for user input
-prompt = st.text_area("Enter your question here:")
 
-# Button to submit the question
-if st.button("Submit", key="submit_button"):
-    if uploaded_file is not None:
-        st.write("🧠 thinking...")
-        response = agent.run(prompt + " " + text)
-        st.write(response)
-    else:
-        st.warning("Please upload a PDF file first.")
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with multiple PDFs",
+                       page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Chat with multiple PDFs :books:")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
+
+
+if __name__ == '__main__':
+    main()
